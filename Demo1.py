86e20972fd24dafa48cf7e5ce6d0efb6b847f879
BhagyaB97 <36728950+BhagyaB97@users.noreply.github.com>
2021-12-10 14:45:38 +0530

    Update Demo1.py

--- Demo1.py
+++ Demo1.py
@@ -1,2 +1,17 @@
 x= input("Enter the number : ")
 print ("The Number is:", x)
+flag = False
+if x > 1:
+    # check for factors
+    for i in range(2, x):
+        if (x % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+if flag:
+    print(x, "is not a prime number")
+else:
+    print(x, "is a prime number")
